c1f3389520bac61db18dc8fdc84ce005050b7197
Waleed Robertson <waleedrbrtsn1@email.com>
2024-08-26 16:20:45 -0400

    tested get restaurant id and update restaurant

--- controllers/restaurant.js
+++ controllers/restaurant.js
@@ -1,11 +1,16 @@
 import express from "express"
 const router = express.Router()
+import Restaurant from "../models/restaurant.js" 
 
 router.get("/:restaurantId", async (req, res) => {
-    res.send("Displays the restaurant's page")
+    const id = req.params.restaurantId
+    const restaurant = await Restaurant.findById(id)
+    res.send(`this is the restaurant's information ${restaurant}`)
 })
 router.put("/:restaurantId/edit", async (req, res) => {
-    res.send("edit what the customer sees")
+    const id = req.params.restaurantId
+    const restaurant = await Restaurant.findByIdAndUpdate(id, { name: "chilis", about: "mid food"})
+    res.send(`edit what the customer sees ${restaurant.name} ${restaurant.about}`)
 })
 router.post("/:restaurantId/reservations/new", async (req, res) => {
     res.send("add reservation")
